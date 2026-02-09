39cfc685914d0b7a604f4bb7172acbcb68e4d652
bbscoding <batuhan5534@gmail.com>
2026-02-09 22:12:55 +0300

    fetchingDataForStarter

---
+++ src/components/AvailablePlaces.jsx
@@ -0,0 +1,27 @@
+import { useState, useEffect } from 'react';
+import Places from './Places.jsx';
+
+export default function AvailablePlaces({ onSelectPlace }) {
+  const [availableplaces, setAvailablePlaces] = useState([]);
+
+  useEffect(() => {
+    async function fetchPlaces() {
+      const response = await fetch('http://localhost:3000/places')
+      const resData = await response.json()
+      setAvailablePlaces(resData.places)
+    }
+
+    fetchPlaces();
+  }, [])
+
+
+
+  return (
+    <Places
+      title="Available Places"
+      places={availableplaces}
+      fallbackText="No places available."
+      onSelectPlace={onSelectPlace}
+    />
+  );
+}
