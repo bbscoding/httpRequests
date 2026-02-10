dc93e4adbd4c68ac150d7448fcf6844eb88ebe08
bbscoding <batuhan5534@gmail.com>
2026-02-10 22:24:28 +0300

    Updating (in a optimistic way)

--- src/http.js
+++ src/http.js
@@ -7,3 +7,21 @@ export async function fetchAvailablePlaces() {
 
     return resData.places
 }
+
+export async function updateUserPlaces(places) {
+    const response = await fetch('http://localhost:3000/user-places', {
+        method: 'PUT',
+        body: JSON.stringify({ places }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    const resData = await response.json();
+    console.log(resData)
+    if (!response.ok) {
+        throw new Error('Failed to update user data.')
+    }
+
+    return resData.message;
+}
