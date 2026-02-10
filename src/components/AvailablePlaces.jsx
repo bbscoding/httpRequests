60ced07b599f5e0af6ba2cc9279bdcd110d29618
bbscoding <batuhan5534@gmail.com>
2026-02-10 21:25:30 +0300

    Modularize

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react';
 import Places from './Places.jsx';
 import ErrorPage from './ErrorPage.jsx';
+import { sortPlacesByDistance } from '../loc.js';
+import { fetchAvailablePlaces } from '../http.js';
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isLoading, setIsLoading] = useState(true)
@@ -11,16 +13,20 @@ export default function AvailablePlaces({ onSelectPlace }) {
     async function fetchPlaces() {
       setIsLoading(true)
       try {
-        const response = await fetch('http://localhost:3000/places')
-        const resData = await response.json()
-        setAvailablePlaces(resData.places)
-        if (!response.ok) {
-          throw new Error('Could not fetch places');
-        }
+        const places = await fetchAvailablePlaces()
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPlaces = sortPlacesByDistance(
+            places,
+            position.coords.latitude,
+            position.coords.longitude
+          )
+          setAvailablePlaces(sortedPlaces)
+          setIsLoading(false)
+        });
       } catch (error) {
         setError({ message: error.message || "No data " })
+        setIsLoading(false)
       }
-      setIsLoading(false)
     }
     fetchPlaces();
   }, [])
